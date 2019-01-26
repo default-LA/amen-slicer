7dcb2a22591def770ddba6f9cb7fbc24d441b1c6
Lloyd Allen <lloyd.g.allen@gmail.com>
2019-01-26 15:36:09 +0000

    add timing array for new breaks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var s = 0;
 var t = 0;
 var playing = false;
-var bpm = 160;    
+var bpm = 180;    
 var SecBpm = 160;    
 var div = 17;             
 var tickTimeout = null;
@@ -9,6 +9,9 @@ var modTimeout = null;
 var markTimeout = null;
 var multiplier = 60;
 var base = 60;
+var tLength = 0;
+var constPos = 0;
+var metroTick = new Howl({src: ["assets/sound/metro.ogg"]});
 var aDown = false;
 var sDown = false;
 var dDown = false;
@@ -28,6 +31,12 @@ var vDown = false;
 var soundPlaying = "";
 var metronome = false;
 var playbackRate = 1;
+var l = 0;
+var soundSelected = "break1";
+var loadedSound = "break1";
+var soundArr = [];
+var soundState = [];
+
 var constMarker = document.getElementById("const-marker"); 
 var modMarker = document.getElementById("mod-marker"); 
 var butIn = document.getElementById("inner"); 
@@ -35,12 +44,6 @@ var butOut = document.getElementById("control");
 var metLed = document.getElementById("metro-led"); 
 var playBtn = document.getElementById("play"); 
 var stopBtn = document.getElementById("stop");
-
-var tLength = 0;
-
-var constPos = 0;
-
-var metroTick = new Howl({src: ["assets/sound/metro.ogg"]});
 
 var waveSq = document.getElementsByClassName("wave");
 var aKey = document.querySelector(".control.a");
@@ -60,6 +63,8 @@ var zKey = document.querySelector(".control.z");
 var xKey = document.querySelector(".control.x");
 var cKey = document.querySelector(".control.c");
 var vKey = document.querySelector(".control.v");
+var playBtn = document.querySelector(".control.seek-btns.play");
+var stopBtn = document.querySelector(".control.seek-btns.stop");
 var aIn = document.querySelector(".inner.a");
 var sIn = document.querySelector(".inner.s");
 var dIn = document.querySelector(".inner.d");
@@ -77,48 +82,56 @@ var zIn = document.querySelector(".inner.z");
 var xIn = document.querySelector(".inner.x");
 var cIn = document.querySelector(".inner.c");
 var vIn = document.querySelector(".inner.v");
-
-var l = 0;
-var soundSelected = "break1";
-
-var soundArr = [];
-
-var soundState = []
+var playIn = document.querySelector(".inner.seek-inner.play");
+var stopIn = document.querySelector(".inner.seek-inner.stop");
+var metroSw = document.querySelector(".metro-switch");
+var waveBg = document.querySelector(".wave");
 
 function loadSound(){
 	if(soundSelected === "break1"){
+		loadedSound = "break1";
 		soundState = [true, false, true, false, true, false, true, true, true, true, true, true, true, false, true, true];
 		soundArr = [];
 		for (var i = 0; i <= 11; i++){
   			soundArr[i] = new Howl({src: ["assets/sound/amen/amen_" + i + ".ogg"]});
   		}
   		tLength = 12;
+  		waveBg.style.backgroundImage = "url('./assets/img/waveform_1i.png')"; 
 	}
+
 	if(soundSelected === "break2"){
+		loadedSound = "break2";
 		soundState = [true, false, true, false, true, false, true, true, true, false, true, false, true, true, true, false];
 		soundArr = [];
 		for (var i = 0; i <= 10; i++){
   			soundArr[i] = new Howl({src: ["assets/sound/break2/break2_" + i + ".ogg"]});
   		}
   		tLength = 10;
+  		waveBg.style.backgroundImage = "url('./assets/img/waveform_2i.png')"; 
 	}
+
 	if(soundSelected === "break3"){
-		soundState = [true, false, true, false, true, false, true, true, true, true, true, true, true, false, true, true];
+		loadedSound = "break3";
+		soundState = [true, false, true, false, true, true, true, true, true, true, true, false, true, true, true, true];
 		soundArr = [];
-		for (var i = 0; i <= 11; i++){
-  			soundArr[i] = new Howl({src: ["assets/sound/amen/amen_" + i + ".ogg"]});
+		for (var i = 0; i <= 12; i++){
+  			soundArr[i] = new Howl({src: ["assets/sound/break3/break3_" + i + ".ogg"]});
   		}
+  		tLength = 13;
+  		waveBg.style.backgroundImage = "url('./assets/img/waveform_3i.png')"; 
 	}
+
 	if(soundSelected === "break4"){
-		soundState = [true, false, true, false, true, false, true, true, true, true, true, true, true, false, true, true];
+		loadedSound = "break4";
+		soundState = [true, false, true, false, true, false, true, true, true, true, true, false, true, false, true, false];
 		soundArr = [];
-		for (var i = 0; i <= 11; i++){
-  			soundArr[i] = new Howl({src: ["assets/sound/amen/amen_" + i + ".ogg"]});
+		for (var i = 0; i <= 9; i++){
+  			soundArr[i] = new Howl({src: ["assets/sound/break4/break4_" + i + ".ogg"]});
   		}
+  		tLength = 10;
+  		waveBg.style.backgroundImage = "url('./assets/img/waveform_4i.png')"; 
 	}
-
 };
-
 loadSound();
 
 function constLoop(){
@@ -136,6 +149,7 @@ function constLoop(){
 				}, 100);
 			}
 		}
+		
 		if(soundState[s] === true){
 			playing = true;
 			soundArr[t].play();
@@ -167,22 +181,18 @@ function constLoop(){
 	}
 	if (s >= 16){
 		reset();
-	}
-
-
-	
+	}	
 }
 
 function markLoop(){
-	if (constPos <= 1200){
+	if (constPos <= 900){
 		constMarker.style.left = constPos + 'px';
-		constPos += 3;
+		constPos += 7;
 	} else {
 		constPos = 0;		
 	}
 	
-	markTimeout = setTimeout(markLoop, 15000 / 60 / bpm); 
-
+	markTimeout = setTimeout(markLoop, 15000 /  bpm / 8) ; 
 }
 
 function modLoop(){
@@ -191,10 +201,13 @@ function modLoop(){
 		soundPlaying.play();		
 	}
 	modTimeout = setTimeout(modLoop, 15000 / secBpm); 
-
 }
 
 function start(){
+	if (playing === true){
+		return;
+	}
+	playing = true;
 	constPos = 0;		
 
 	markLoop();
@@ -206,12 +219,20 @@ function stop(){
 	playing = false;
 	clearTimeout(tickTimeout);
 	clearTimeout(modTimeout);
+	clearTimeout(markTimeout);
+	s = 0;
+	t = 0;
 }
 
 function reset(){
 	clearTimeout(tickTimeout);
 	clearTimeout(modTimeout);
 	clearTimeout(markTimeout);
+
+	if (loadedSound != soundSelected){
+		loadSound();
+	}
+	playing = false;
 	start();
 }
 
@@ -226,12 +247,14 @@ document.addEventListener('keydown', function(event) {
 		    aDown = true;
 		}
 	} 
+
 	if (event.code == 'KeyS') {
   		if(sDown) return;
   		sKey.classList.add("control-down");	
 		sIn.classList.add("inner-down");
     	sDown = true;
   	} 
+
   	if (event.code == 'KeyD') {
   		if(dDown) return;
   		dKey.classList.add("control-down");	
@@ -239,6 +262,7 @@ document.addEventListener('keydown', function(event) {
 	    secBpm = secBpm * 2;	 	    
     	dDown = true;
   	} 
+
   	if (event.code == 'KeyF') {
   		if(fDown) return;
   		fKey.classList.add("control-down");	
@@ -246,6 +270,7 @@ document.addEventListener('keydown', function(event) {
 	    secBpm = secBpm * 4;	 	    
     	fDown = true;
   	} 
+
   	if (event.code == 'KeyG') {
   		if(fDown) return;
   		gKey.classList.add("control-down");	
@@ -253,6 +278,7 @@ document.addEventListener('keydown', function(event) {
 	    secBpm = secBpm * 0.33333;	 	    
     	fDown = true;
   	} 
+
   	if (event.code == 'KeyJ') {
   		if(soundPlaying._rate < 0.4) {
   			return;
@@ -272,6 +298,7 @@ document.addEventListener('keydown', function(event) {
 		kIn.classList.add("inner-down");
     	kDown = true;
   	} 
+
   	if (event.code == 'KeyL') {
 	    soundPlaying._rate -= 0.01;  		
   		if(kDown) return;  		
@@ -280,19 +307,18 @@ document.addEventListener('keydown', function(event) {
 		lIn.classList.add("inner-down");
   		lDown = true;
   	} 
+
 	if (event.code == 'KeyO') {
   		if(playbackRate < 0.4) {
   			return;
   		} else {
 	  		playbackRate -= 0.05;
-	  		console.log(playbackRate);
 	    }
 	    if(oDown) return;
 	    oKey.classList.add("control-down");	
 		oIn.classList.add("inner-down");
 		oDown = true;  		
   	}
-
 
   	if (event.code == 'KeyP') {
   		if(playbackRate > 3) {
@@ -304,7 +330,7 @@ document.addEventListener('keydown', function(event) {
 	    pKey.classList.add("control-down");	
 		pIn.classList.add("inner-down");
 		pDown = true;
-  	} 
+  	}
 
   	if (event.code == 'KeyQ') {
   		if(bpm <= 60) {
@@ -317,12 +343,12 @@ document.addEventListener('keydown', function(event) {
 		qIn.classList.add("inner-down");
 		qDown = true;
   	} 
+
   	if (event.code == 'KeyW') {
   		if(playbackRate >= 250) {
   			return;
   		} else {
 	  		bpm++;
-
 	  		//code for marker speed too
 	    }	   
 	    if(wDown) return;
@@ -330,34 +356,39 @@ document.addEventListener('keydown', function(event) {
 		wIn.classList.add("inner-down");
 		wDown = true;
   	} 
+
   	if (event.code == 'KeyZ') {
 	    if(zDown) return;
 	    zKey.classList.add("control-down");	
 		zIn.classList.add("inner-down");
 		zDown = true;
 		soundSelected = "break1";
-		loadSound();
 
   	} 
+
   	if (event.code == 'KeyX') {
 	    if(xDown) return;
 	    xKey.classList.add("control-down");	
 		xIn.classList.add("inner-down");
 		xDown = true;
 		soundSelected = "break2";
-		// loadSound();
   	} 
+
   	if (event.code == 'KeyC') {
 	    if(cDown) return;
 	    cKey.classList.add("control-down");	
 		cIn.classList.add("inner-down");
 		cDown = true;
+		soundSelected = "break3";
+
   	} 
+
   	if (event.code == 'KeyV') {
 	    if(vDown) return;
 	    vKey.classList.add("control-down");	
 		vIn.classList.add("inner-down");
 		vDown = true;
+		soundSelected = "break4";
   	} 
 });
 
@@ -367,80 +398,92 @@ document.addEventListener('keyup', function(event) {
 		aIn.classList.remove("inner-down");	
 		aDown = false;
   	} 
+
   	if (event.code == 'KeyS') {
   		sKey.classList.remove("control-down");
 		sIn.classList.remove("inner-down");	
     	sDown = false;
 	} 
+
 	if (event.code == 'KeyD') {
 		dKey.classList.remove("control-down");
 		dIn.classList.remove("inner-down");	
     	dDown = false;
-
 	} 
+
 	if (event.code == 'KeyF') {
 		fKey.classList.remove("control-down");
 		fIn.classList.remove("inner-down");	
     	fDown = false;
 	} 
+
 	if (event.code == 'KeyG') {
 		gKey.classList.remove("control-down");
 		gIn.classList.remove("inner-down");	
     	fDown = false;
-	} 
+	}
+
 	if (event.code == 'KeyJ') {
 		jKey.classList.remove("control-down");
 		jIn.classList.remove("inner-down");
     	jDown = false;
 	} 
-
 	if (event.code == 'KeyK') {
 		kKey.classList.remove("control-down");
 		kIn.classList.remove("inner-down");	
     	kDown = false;
 
 	} 
+
 	if (event.code == 'KeyL') {
 		lKey.classList.remove("control-down");
 		lIn.classList.remove("inner-down");	
     	lDown = false;
     	l = 0;
 	} 
+
 	if (event.code == 'KeyO') {
 		oKey.classList.remove("control-down");
 		oIn.classList.remove("inner-down");	
     	oDown = false;
 	} 
+
 	if (event.code == 'KeyP') {
 		pKey.classList.remove("control-down");
 		pIn.classList.remove("inner-down");	
     	pDown = false;
-	} 
+	}
+
 	if (event.code == 'KeyQ') {
 		qKey.classList.remove("control-down");
 		qIn.classList.remove("inner-down");	
     	qDown = false;
 	} 
+
 	if (event.code == 'KeyW') {
 		wKey.classList.remove("control-down");
 		wIn.classList.remove("inner-down");	
     	wDown = false;
 	} 
+
 	if (event.code == 'KeyZ') {
 		zKey.classList.remove("control-down");
 		zIn.classList.remove("inner-down");	
     	zDown = false;
 	} 
+
 	if (event.code == 'KeyX') {
 		xKey.classList.remove("control-down");
 		xIn.classList.remove("inner-down");	
     	xDown = false;
 	} 
+
 	if (event.code == 'KeyC') {
 		cKey.classList.remove("control-down");
 		cIn.classList.remove("inner-down");	
     	cDown = false;
 	} 
+	
 	if (event.code == 'KeyV') {
 		vKey.classList.remove("control-down");
 		vIn.classList.remove("inner-down");	
@@ -450,8 +493,29 @@ document.addEventListener('keyup', function(event) {
 
 playBtn.addEventListener("click", function(){
 	start();
+	playBtn.classList.add("control-down");	
+	// playIn.classList.add("inner-down");
 });
 
-stopBtn.addEventListener("click", function(){
+stopBtn.addEventListener("mousedown", function(){
 	stop();
+	playBtn.classList.remove("control-down");	
+	// playIn.classList.remove("inner-down");
+	stopBtn.classList.add("control-down");	
+	// stopIn.classList.add("inner-down");
+});
+
+stopBtn.addEventListener("mouseup", function(){
+	stopBtn.classList.remove("control-down");	
+	// stopIn.classList.remove("inner-down");
+});
+
+metroSw.addEventListener("click", function(){
+	if (metronome === true){
+		metronome = false;
+		this.classList.remove("met-switch-in");		
+	} else {
+		metronome = true;
+		this.classList.add("met-switch-in");
+	}
 });
